a0ae68393dba3cca1e52b93926144da301e7f104
prodakt <bioinformatyka@gmail.com>
2023-02-28 12:33:42 +0100

    rbind(data.frame(tmp = cm$overall), data.frame(tmp = cm$byClass))

--- R/BestTool.R
+++ R/BestTool.R
@@ -34,7 +34,7 @@ BestTool <- function(BestPat, tools){
     cm <- confusionMatrix(data = dat,
                           reference = ref,
                           mode = "prec_recall", positive = "1")
-    cm <- rbind(assign(i,data.frame(cm$overall)), assign(i,data.frame(cm$byClass)))
+    cm <- rbind(data.frame(tmp = cm$overall), data.frame(tmp = cm$byClass))
     #    cm <- data.frame(cm$overall)
     cm <- round(cm, 4)
     BP.cpt <- cbind(BP.cpt,cm)
