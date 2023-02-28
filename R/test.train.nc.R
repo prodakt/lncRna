a8461f9d0fa6634bb0ce71e7ae704bd19656e0e9
prodakt <bioinformatyka@gmail.com>
2023-02-28 12:08:53 +0100

    update man test.train

--- R/test.train.nc.R
+++ R/test.train.nc.R
@@ -1,8 +1,8 @@
 #' test.train.nc function
 #'
-#' This function allows you to split a sequences (from FASTA file) into two sets: test and train. As a result, the function outputs a list consisting of two elements "test" and "train" containing sequence names.
+#' This function allows you to split a sequences \(from FASTA file\) into two sets: test and train. As a result, the function outputs a list consisting of two elements "test" and "train" containing sequence names.
 #' @param nc.fa the variable containing sequences in fasta format (from read.fasta() function from seqinr library)
-#' @param percent_train the ratio value to divide the input set (he default is 0.6, which means that 60% of the sequence is to be allocated to the training set)
+#' @param percent_train the ratio value to divide the input set (he default is 0.6, which means that 60\% of the sequence is to be allocated to the training set)
 #' @keywords training test proportion lncRNA
 #' @export
 #' @examples
