8b1f0f9081408e89d42e55ae902bf8dc8089961b
prodakt <bioinformatyka@gmail.com>
2023-03-08 15:19:19 +0100

    Add helper functions

---
+++ R/helpFun.R
@@ -0,0 +1,27 @@
+#' %!in% function
+#'
+#' This function is negation of %in%.
+#' @param x vector of values to be filtered
+#' @param y vector of values to eliminate
+#' @keywords negate
+#' @export
+#' @examples
+#' vector_A %!in% vector_B
+#'
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+#' h.t function
+#'
+#' Display head and tail together.
+#' @param table table
+#' @param n number of rows to display
+#' @keywords table head tail
+#' @export
+#' @examples
+#' h.t(table)
+#'
+h.t <- function(table, n=3) {
+  tbl.tmp <- rbind(head(table,n),"..." = rep("...", ncol(table)) ,tail(table,n))
+  return(tbl.tmp)
+}
