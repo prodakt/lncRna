2865cb5a7e8151ba58306e8ca039d7e2946c092e
prodakt <bioinformatyka@gmail.com>
2023-03-08 15:24:11 +0100

    update helper

--- R/helpFun.R
+++ R/helpFun.R
@@ -1,15 +1,3 @@
-#' \!in function
-#'
-#' This function is negation of "contains.
-#' @param x vector of values to be filtered
-#' @param y vector of values to eliminate
-#' @keywords negate
-#' @export
-#' @examples
-#'
-
-'%!in%' <- function(x,y)!('%in%'(x,y))
-
 #' h.t function
 #'
 #' Display head and tail together.
